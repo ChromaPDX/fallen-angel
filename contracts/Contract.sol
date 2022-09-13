4e4f90950852706051493afe129448fe469ddfcf
adam wong <adamwong246@gmail.com>
2022-09-12 18:23:11 -0700

    try non enum version

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,23 +1,9 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@thirdweb-dev/contracts/base/ERC721SignatureMint.sol";
 
-import "@thirdweb-dev/contracts/eip/interface/IERC721Enumerable.sol";
-
-contract Contract is ERC721SignatureMint, IERC721Enumerable {
-    // Mapping from owner to list of owned token IDs
-    mapping(address => mapping(uint256 => uint256)) private _ownedTokens;
-
-    // Mapping from token ID to index of the owner tokens list
-    mapping(uint256 => uint256) private _ownedTokensIndex;
-
-    // Array with all token ids, used for enumeration
-    uint256[] private _allTokens;
-
-    // Mapping from token id to position in the allTokens array
-    mapping(uint256 => uint256) private _allTokensIndex;
-
+contract Contract is ERC721SignatureMint {
     constructor(
         string memory _name,
         string memory _symbol,
@@ -34,36 +20,7 @@ contract Contract is ERC721SignatureMint, IERC721Enumerable {
         )
     {}
 
-    // /**
-    //  * @dev See {IERC721Enumerable-totalSupply}.
-    //  */
-    // function totalSupply() public view virtual override returns (uint256) {
-    //     return _allTokens.length;
-    // }
-
-    function tokenByIndex(uint256 _index)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        // require(
-        //     _index < Contract.totalSupply(),
-        //     "Contract: global index out of bounds"
-        // );
-        return _allTokens[_index];
-    }
-
-    function tokenOfOwnerByIndex(address _owner, uint256 _index)
-        external
-        view
-        override
-        returns (uint256)
-    {
-        // require(
-        //     _index < ERC721SignatureMint.balanceOf(owner),
-        //     "Contract: owner index out of bounds"
-        // );
-        return _ownedTokens[_owner][_index];
+    function walletHoldsToken(address _wallet) public view returns (bool) {
+        return ERC721SignatureMint(address(this)).balanceOf(_wallet) > 0;
     }
 }
