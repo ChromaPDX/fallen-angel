121fcf001b4b68929aa4b7b1c81c4a1937d1c0ba
adam wong <adamwong246@gmail.com>
2022-10-24 11:14:35 -0700

    switch back to goerli

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ export default {
   owner: "0xB1e03503AB2C0159a4CeF0f9b4703292C221eF45",
   stripeCheckoutLink: "https://buy.stripe.com/5kA8xs4Tp4SkehafYY",
 
-  // alchemyKey: "yxZEOqdqzCNCExJZEwY4iPEeu6jZpyMi",
-  // chain: chain.goerli,
+  alchemyKey: "yxZEOqdqzCNCExJZEwY4iPEeu6jZpyMi",
+  chain: chain.goerli,
 
-  alchemyKey: "577rlGmGIdps3j2owCjazI3jPeYrgvNt",
-  chain: chain.mainnet
+  // alchemyKey: "577rlGmGIdps3j2owCjazI3jPeYrgvNt",
+  // chain: chain.mainnet
 }
