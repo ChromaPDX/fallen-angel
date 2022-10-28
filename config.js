3612d92e56b46d26d7f32e04da8d7ad07ee05121
adam wong <adamwong246@gmail.com>
2022-10-28 15:56:36 -0700

    allowlist proof

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ module.exports = {
   paperCheckoutLink: "https://paper.xyz/checkout/5174045b-e437-464a-8e6e-5e7d943ca48b",
 
   allowListProof: [
-    '0x97533c02110e6573027c4c00710b22618e3b039a5a99f419e86c29e537c4b59c',
-    '0xeafac40d278a20912dad05fa5ea3b0c8489d4b9e382f8925e343ea6d7986b376'
+    '0xeafac40d278a20912dad05fa5ea3b0c8489d4b9e382f8925e343ea6d7986b376',
+    '0x8f91e3c7e7c77e442ed2c098c63df34523c5cc25f1e7cc7c1b426485b79092e5'
   ],
 
   // development
--- src/allowlistproof.js
+++ src/allowlistproof.js
@@ -3,8 +3,11 @@ const secrets = require('../.secrets.json');
 const configs = require('../config.js');
 
 const main = async () => {
+  console.log("hello")
   const sdk = ThirdwebSDK.fromPrivateKey(secrets.privateKey, configs.chain.network);
+  console.log("mnark0", configs.contractAddress)
   const contract = await sdk.getContract(configs.contractAddress);
+  console.log("mnark1")
   // console.log((await contract.erc721.getClaimTransaction()).args)
   console.log((await contract.erc721.getClaimTransaction()).args[4])
 }
