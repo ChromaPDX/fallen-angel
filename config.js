f14654e0e75f5bb0ba86886383c48d9c589cbbff
adam wong <adamwong246@gmail.com>
2022-11-09 13:39:11 -0800

    refactor to user thirdwebsdk. allow lists work!

--- config.js
+++ config.js
@@ -18,10 +18,11 @@ module.exports = {
 
   currency: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
 
-  contractAddress: "0xA8a4764FD41b036D4B00795bE31E0eba92ef869d",
+  // contractAddress: "0xA8a4764FD41b036D4B00795bE31E0eba92ef869d",
+  contractAddress: "0x83D70B516D691e2518bebcc4825ccf98f5a08d7C",
 
-  // owner: "0xB1e03503AB2C0159a4CeF0f9b4703292C221eF45",
-  owner: "0x0A58e58ECb0dD36E0e56975b86F9d76Dd9e4272e",
+  owner: "0xB1e03503AB2C0159a4CeF0f9b4703292C221eF45",
+  // owner: "0x0A58e58ECb0dD36E0e56975b86F9d76Dd9e4272e",
 
 
 
@@ -31,11 +32,11 @@ module.exports = {
   ],
 
   // development
-  // alchemyKey: "yxZEOqdqzCNCExJZEwY4iPEeu6jZpyMi",
-  chain: chain.mainnet,
+  alchemyKey: "yxZEOqdqzCNCExJZEwY4iPEeu6jZpyMi",
+  chain: chain.goerli,
 
   // production
-  alchemyKey: "577rlGmGIdps3j2owCjazI3jPeYrgvNt",
+  // alchemyKey: "577rlGmGIdps3j2owCjazI3jPeYrgvNt",
   // chain: chain.mainnet
 
   stripeCheckoutLink: "https://buy.stripe.com/5kA8xs4Tp4SkehafYY",
--- index.tsx
+++ index.tsx
@@ -1,22 +1,125 @@
 require("@rainbow-me/rainbowkit/styles.css");
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom";
+
+import configs from "./config";
+
+// import {
+
+// } from "@thirdweb-dev/react";
 
 import { Checkout } from "./src/Checkout";
 import { Redeem } from "./src/Redeem";
 import AppFrame from "./src/AppFrame";
 
-const SignerTest = (props: any) => {
-  return <AppFrame >
-    {
-      (contract, signer, address): React.ReactNode => <>
-        <Checkout contract={contract} signer={signer} address={address} />
-        <hr />
-        <Redeem contract={contract} signer={signer} address={address} />
-      </>
+import {
+
+  useContract,
+  useContractRead,
+  useContractWrite,
+  ChainId, ThirdwebProvider, ConnectWallet,
+  useAddress,
+  ThirdwebNftMedia, useNFTs, useAccount
+} from "@thirdweb-dev/react";
+import { NFT } from "@thirdweb-dev/sdk";
+
+function Home() {
+  const { contract, isLoading: contractIsLoading } = useContract(configs.contractAddress);
+  const { data: nfts, isLoading: isReadingNfts } = useNFTs(contract);
+  const account = useAccount();
+
+  const myAddress = account[0].data?.address;
+
+  const [state, setState] = useState<{ stage: number, myNfts: NFT[] }>({
+    stage: 0,
+    myNfts: []
+  });
+
+  useEffect(() => {
+    if (myAddress && state.stage === 0) {
+      contract?.erc721.getOwned(myAddress).then((nfts) => {
+        setState({
+          stage: 1,
+          myNfts: nfts
+        });
+      })
     }
-  </AppFrame>
+  })
+
+  return (
+    <div>
+      <h2>My NFTs</h2>
+
+      {(state.myNfts || []).map((nft) => (
+        <ThirdwebNftMedia
+          key={nft.metadata.id}
+          metadata={nft.metadata}
+          height={"200"}
+        />
+      ))}
+
+
+
+      <hr />
+
+
+      <button onClick={(e) => {
+        contract?.erc721.claim(1)
+      }}>claim</button>
+
+      <h2>All NFTs</h2>
+      {isReadingNfts ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          {nfts?.map((nft) => (
+            <ThirdwebNftMedia
+              key={nft.metadata.id}
+              metadata={nft.metadata}
+              height={"200"}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+const YourApp = () => {
+  const address = useAddress();
+  return (
+    <div>
+      <ConnectWallet />
+      <Home />
+    </div>
+  );
+};
+
+const LiquidCollectionReactApp = (props: any) => {
+  return (<ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <YourApp />
+
+    {/* <AppFrame >
+      {
+        (contract, signer, address): React.ReactNode => <>
+          <Checkout contract={contract} signer={signer} address={address} />
+          <hr />
+          <Redeem contract={contract} signer={signer} address={address} />
+        </>
+      }
+    </AppFrame> */}
+
+  </ThirdwebProvider>);
+  // return <AppFrame >
+  //   {
+  //     (contract, signer, address): React.ReactNode => <>
+  //       <Checkout contract={contract} signer={signer} address={address} />
+  //       <hr />
+  //       <Redeem contract={contract} signer={signer} address={address} />
+  //     </>
+  //   }
+  // </AppFrame>
 };
 document.addEventListener("DOMContentLoaded", (event) =>
-  ReactDom.render(<SignerTest />, document.getElementById('root')));
+  ReactDom.render(<LiquidCollectionReactApp />, document.getElementById('root')));
