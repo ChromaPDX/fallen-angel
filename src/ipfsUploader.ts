3baa6f85538f2c070b0ee7b8eb10f1925122cee0
adam wong <adamwong246@gmail.com>
2022-11-16 18:57:21 -0800

    ipfs uploader

--- src/ipfsUploader.ts
+++ src/ipfsUploader.ts
@@ -1,23 +1,22 @@
 import { filesFromPath } from "files-from-path";
 import { NFTStorage } from 'nft.storage'
 import fs from "fs";
+import path from "path";
 
-
-const NFT_STORAGE_TOKEN = JSON.parse(fs.readFileSync("./.secrets.json").toString()).nftStorageApiKey;
+if (!process.env.nft_dot_storage_apikey) {
+  console.error("set an env var for `nft_dot_storage_apikey`!");
+  process.exit(-1)
+}
+const NFT_STORAGE_TOKEN = process.env.nft_dot_storage_apikey;
 
 console.log("hello ipfs uploader")
 
-const files = filesFromPath('./src/nfts/batch1/pre', {
-  pathPrefix: 'pre', //path.resolve(directoryPath), // see the note about pathPrefix below
+const files = filesFromPath('./src/nfts/batch3/images', {
+  pathPrefix: path.resolve('./src/nfts/batch3/images'), // see the note about pathPrefix below
   // hidden: true, // use the default of false if you want to ignore files that start with '.'
 })
 
-console.log(files)
 const storage = new NFTStorage({ token: NFT_STORAGE_TOKEN })
-
-// console.log(`storing file(s) from ${path}`)
 const cid = await storage.storeDirectory(files)
-console.log({ cid })
 
-const status = await storage.status(cid)
-console.log(status)
+console.log("done", { cid })
