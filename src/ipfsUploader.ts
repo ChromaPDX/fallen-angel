a2287893a0c87d9b761d1a3555f91ca54ac02e88
adam wong <adamwong246@gmail.com>
2022-11-25 14:02:04 -0800

    add assets

--- src/ipfsUploader.ts
+++ src/ipfsUploader.ts
@@ -1,22 +1,22 @@
 import { filesFromPath } from "files-from-path";
-import { NFTStorage } from 'nft.storage'
+import { NFTStorage } from "nft.storage";
 import fs from "fs";
 import path from "path";
 
 if (!process.env.nft_dot_storage_apikey) {
   console.error("set an env var for `nft_dot_storage_apikey`!");
-  process.exit(-1)
+  process.exit(-1);
 }
 const NFT_STORAGE_TOKEN = process.env.nft_dot_storage_apikey;
 
-console.log("hello ipfs uploader")
+console.log("hello ipfs uploader");
 
-const files = filesFromPath('./src/nfts/batch3/images', {
-  pathPrefix: path.resolve('./src/nfts/batch3/images'), // see the note about pathPrefix below
+const files = filesFromPath("./src/nfts/fallen-angel/images", {
+  pathPrefix: path.resolve("./src/nfts/fallen-angel/images"), // see the note about pathPrefix below
   // hidden: true, // use the default of false if you want to ignore files that start with '.'
-})
+});
 
-const storage = new NFTStorage({ token: NFT_STORAGE_TOKEN })
-const cid = await storage.storeDirectory(files)
+const storage = new NFTStorage({ token: NFT_STORAGE_TOKEN });
+const cid = await storage.storeDirectory(files);
 
-console.log("done", { cid })
+console.log("done", { cid });
--- src/lazyminter.ts
+++ src/lazyminter.ts
@@ -5,13 +5,11 @@ import configs from "../config";
 
 if (!process.env.privatekey) {
   console.error("set an env var for `privatekey`!");
-  process.exit(-1)
+  process.exit(-1);
 }
 const PRIVATE_KEY = process.env.privatekey;
 
 const sdk = ThirdwebSDK.fromPrivateKey(PRIVATE_KEY, configs.chain.network);
-
-
 
 // const metadatas = [{
 //   name: "Cool NFT",
@@ -23,12 +21,12 @@ const sdk = ThirdwebSDK.fromPrivateKey(PRIVATE_KEY, configs.chain.network);
 //   // image: fs.readFileSync("path/to/image.png"),
 // }];
 
-const metadatas = new Array(5).fill({})
+const metadatas = new Array(20).fill({});
 
 const contract = await sdk.getContract(configs.contractAddress);
-const results = await contract.erc721.lazyMint(metadatas)
+const results = await contract.erc721.lazyMint(metadatas);
 
-console.log("done", results)
+console.log("done", results);
 // import { NFTStorage } from 'nft.storage'
 
 // import path from "path";
